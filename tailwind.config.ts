3301a1dd48d86cdfc7d04fd76b6b2f3c413b0708
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-23 21:26:16 +0900

    chore: check husky

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-    content: [
-        './src/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
+    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
     theme: {},
     plugins: [],
 }
